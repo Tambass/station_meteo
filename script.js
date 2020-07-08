deabc68dac7df59b41b1bbc9429f7d77d4ae85a9
Tambass <tambass85@gmail.com>
2020-07-08 10:54:25 +0200

    ajout code js pour prévisions

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const miseAJour = document.getElementById("maj");
 miseAJour.innerText = "Mise à jour à " + heure + ":" + minutes;
 //console.log(miseAJour);
 
-
-
 //const city = document.getElementById('city')
 //const queryLoc = document.getElementById('queryLoc').value
 //city.innerText = queryLoc
@@ -29,8 +27,8 @@ $(document).ready(function () {
     // console.log(url);
 
     fetch(url, {
-        method: "get"
-      })
+      method: "get",
+    })
       .then((response) => response.json())
       .then((results) => {
         //console.log(results);
@@ -62,7 +60,6 @@ $(document).ready(function () {
 
 // Fonction résultat météo
 
-
 var callBackGetSuccess = function (data) {
   //console.log("donnees api", data);
   //alert("Meteo temp  : "  + data.main.temp);
@@ -71,72 +68,86 @@ var callBackGetSuccess = function (data) {
 
   var description = document.getElementById("weather-main");
   description.innerHTML = data.weather[0].description;
-  
-//   var icon = document.getElementById("weather-icon");
-//   var logo = data.weather[0].icon;
-//   var urlImage = "http://openweathermap.org/img/wn/"+ logo +"@2x.png"
-//   icon.innerHTML = urlImage
-   
-// console.log(urlImage);
-  
+
+  //   var icon = document.getElementById("weather-icon");
+  //   var logo = data.weather[0].icon;
+  //   var urlImage = "http://openweathermap.org/img/wn/"+ logo +"@2x.png"
+  //   icon.innerHTML = urlImage
+
+  // console.log(urlImage);
+
   // const nuage = "overcast clouds";
   // const soleil ="clear sky";
-//  if(description.innerHTML === nuage){
-//   description.innerHTML = description.innerHTML.replace('overcast clouds', 'très nuageux')
-//  }else if(description.innerHTML === soleil){
-//   description.innerHTML = description.innerHTML.replace('clear sky', 'ciel bleu')
-//  }
- 
-//   console.log(description.innerHTML);
+  //  if(description.innerHTML === nuage){
+  //   description.innerHTML = description.innerHTML.replace('overcast clouds', 'très nuageux')
+  //  }else if(description.innerHTML === soleil){
+  //   description.innerHTML = description.innerHTML.replace('clear sky', 'ciel bleu')
+  //  }
+
+  //   console.log(description.innerHTML);
   console.log(description);
 
   var image = data.weather[0].icon;
-  $("#weather-icon").attr("src", "http://openweathermap.org/img/wn/" + image + ".png");
+  $("#weather-icon").attr(
+    "src",
+    "http://openweathermap.org/img/wn/" + image + ".png"
+  );
 
   // var matin = document.querySelector('.temperature-value');
   // matin.innerText = data.list[0].main.temp;
-
 };
 
-
-var callBackGet = function(list){
-
-  var matin = document.querySelector('.temperature-am');
-  matin.innerText = list.list[0].main.temp; 
+var callBackGet = function (list) {
+  var matin = document.querySelector(".temperature-am");
+  matin.innerText = list.list[0].main.temp;
   var image = list.list[0].weather[0].icon;
-  $("#am-icon").attr("src", "http://openweathermap.org/img/wn/" + image + ".png");
+  $("#am-icon").attr(
+    "src",
+    "http://openweathermap.org/img/wn/" + image + ".png"
+  );
 
-  var apresMidi = document.querySelector('.temperature-pm');
-  apresMidi.innerText = list.list[2].main.temp; 
+  var apresMidi = document.querySelector(".temperature-pm");
+  apresMidi.innerText = list.list[2].main.temp;
   var image = list.list[2].weather[0].icon;
-  $("#pm-icon").attr("src", "http://openweathermap.org/img/wn/" + image + ".png");
+  $("#pm-icon").attr(
+    "src",
+    "http://openweathermap.org/img/wn/" + image + ".png"
+  );
 
-  var soir = document.querySelector('.temperature-soir');
-  soir.innerText = list.list[4].main.temp; 
+  var soir = document.querySelector(".temperature-soir");
+  soir.innerText = list.list[4].main.temp;
   var image = list.list[4].weather[0].icon;
-  $("#soir-icon").attr("src", "http://openweathermap.org/img/wn/" + image + ".png");
+  $("#soir-icon").attr(
+    "src",
+    "http://openweathermap.org/img/wn/" + image + ".png"
+  );
 
-  var nuit = document.querySelector('.temperature-nuit');
-  nuit.innerText = list.list[5].main.temp; 
+  var nuit = document.querySelector(".temperature-nuit");
+  nuit.innerText = list.list[5].main.temp;
   var image = list.list[5].weather[0].icon;
-  $("#nuit-icon").attr("src", "http://openweathermap.org/img/wn/" + image + ".png");
-}
+  $("#nuit-icon").attr(
+    "src",
+    "http://openweathermap.org/img/wn/" + image + ".png"
+  );
+};
 
 function buttonClickGET() {
   var loccity = document.getElementById("loc-city").value;
   var city = document.getElementById("city");
+  var city2 = document.getElementById("ville");
   city.innerText = loccity;
-  var url = ["https://api.openweathermap.org/data/2.5/weather?q=" +
-    loccity +
-    "&lang=fr&appid=b7062e52926ecc78dde9910e256b1067&units=metric",
+  city2.innerText = loccity;
+  var url = [
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      loccity +
+      "&lang=fr&appid=b7062e52926ecc78dde9910e256b1067&units=metric",
 
-    "http://api.openweathermap.org/data/2.5/forecast?q="+
-    loccity + 
-    "&lang=fr&appid=b7062e52926ecc78dde9910e256b1067&units=metric"
-];
-    
-      
-    $.get(url[0], callBackGetSuccess)
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      loccity +
+      "&lang=fr&appid=b7062e52926ecc78dde9910e256b1067&units=metric",
+  ];
+
+  $.get(url[0], callBackGetSuccess)
     .done(function () {
       //alert( "second success" );
     })
@@ -147,9 +158,7 @@ function buttonClickGET() {
       //alert( "finished" );
     });
 
-
-
-    $.get(url[1], callBackGet)
+  $.get(url[1], callBackGet)
     .done(function () {
       //alert( "second success" );
     })
@@ -159,6 +168,4 @@ function buttonClickGET() {
     .always(function () {
       //alert( "finished" );
     });
-    
-
-  }
+}
